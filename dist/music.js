b92d9311b0f886482c36e4784dba42aaa1d1680f
Three-FISH <1119181761@qq.com>
2020-04-09 13:41:03 +0800

    Site updated: 2020-04-09 13:41:01

---
+++ dist/music.js
@@ -0,0 +1,14 @@
+
+const ap = new APlayer({
+    container: document.getElementById('aplayer'),
+    fixed: true,
+    autoplay: false,
+    audio: [
+      {
+        name: "忽然",
+        artist: '李先生',
+        url: 'http://lc-pdocKLdh.cn-n1.lcfile.com/1f52ae95e4e2f25fe809/%E6%9D%8E%E5%BF%97%20-%20%E5%BF%BD%E7%84%B6.mp3',
+        cover: 'http://lc-pdocKLdh.cn-n1.lcfile.com/710213f43c6a14895e88/%E6%9D%8E%E5%BF%97%E3%80%8Ai%EF%BC%8FO%E3%80%8B.jpg',
+      }
+    ]
+});
