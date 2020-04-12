92f3c201170f4a2e050d77a1e64b8d9ca653acd7
Three-FISH <1119181761@qq.com>
2020-04-12 13:55:08 +0800

    Site updated: 2020-04-12 13:55:07

--- dist/music.js
+++ dist/music.js
@@ -5,22 +5,10 @@ const ap = new APlayer({
     autoplay: false,
     audio: [
       {
-        name: "你离开了南京,从此没有人和我说话",
+        name: "寻找+忽然+热河",
         artist: '李先生',
-        url: 'http://lc-pdocKLdh.cn-n1.lcfile.com/12a48059c1f76c009951/%E6%9D%8E%E5%BF%97%20-%20%E4%BD%A0%E7%A6%BB%E5%BC%80%E4%BA%86%E5%8D%97%E4%BA%AC%EF%BC%8C%E4%BB%8E%E6%AD%A4%E6%B2%A1%E6%9C%89%E4%BA%BA%E5%92%8C%E6%88%91%E8%AF%B4%E8%AF%9D%202015%E7%8E%B0%E5%9C%BA%E7%89%88.mp3',
+        url: 'http://lc-pdocKLdh.cn-n1.lcfile.com/174318a0265d4393f535/%E5%AF%BB%E6%89%BE%2B%E5%BF%BD%E7%84%B6%2B%E7%83%AD%E6%B2%B3.mp3',
         cover: 'http://lc-pdocKLdh.cn-n1.lcfile.com/648cae08f82e9559b38d/two.png',
-      },
-      {
-        name: "忽然",
-        artist: '李先生',
-        url: 'http://lc-pdocKLdh.cn-n1.lcfile.com/1f52ae95e4e2f25fe809/%E6%9D%8E%E5%BF%97%20-%20%E5%BF%BD%E7%84%B6.mp3',
-        cover: 'http://lc-pdocKLdh.cn-n1.lcfile.com/7ee269e519d0971dbe42/one.png',
-      },
-      {
-        name: '和你在一起',
-        artist: '李先生',
-        url: 'http://lc-pdocKLdh.cn-n1.lcfile.com/52ce15486428f7c1045e/%E5%92%8C%E4%BD%A0%E5%9C%A8%E4%B8%80%E8%B5%B7.mp3',
-        cover: 'http://lc-pdocKLdh.cn-n1.lcfile.com/da268258128929b2a09c/four.png',
       },
       {
         name: '这个世界会好吗',
@@ -28,11 +16,6 @@ const ap = new APlayer({
         url: 'http://lc-pdocKLdh.cn-n1.lcfile.com/50f80eb97935ea19e0c5/%E6%9D%8E%E5%BF%97%20-%20%E8%BF%99%E4%B8%AA%E4%B8%96%E7%95%8C%E4%BC%9A%E5%A5%BD%E5%90%972015.mp3',
         cover: 'http://lc-pdocKLdh.cn-n1.lcfile.com/9d7f64b15e348bb79fff/five.png',
       },
-      {
-        name: '关于郑州的记忆',
-        artist: '李先生',
-        url: 'http://lc-pdocKLdh.cn-n1.lcfile.com/9ae545528dee4b5db90a/%E6%9D%8E%E5%BF%97%20-%20%E5%85%B3%E4%BA%8E%E9%83%91%E5%B7%9E%E7%9A%84%E8%AE%B0%E5%BF%86.mp3',
-        cover: 'http://lc-pdocKLdh.cn-n1.lcfile.com/91ead4ef6f8fa8233de8/three.png',
-      }
+      
     ]
 });
